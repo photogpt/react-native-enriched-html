0b4bdb17c209a9032e2909c362d0419e2f4af542
Kaami-Sama <work.alamsahil@gmail.com>
2026-08-17 10:02:38 +0530

    fix(ios): declare CheckboxHitTestUtils helpers used by pressable mentions
      - The pressable-mentions feature added calls in TextBlockTapGestureRecognizer.mm to `containerPointFromViewPoint:` and `glyphIndexAtContainerPoint:` on CheckboxHitTestUtils.
      - Both methods were implemented in the .mm but never declared in the .h, breaking the iOS build with "no known class method" / "no viable conversion from 'id'" errors.
      - Add the missing declarations.

--- ios/utils/CheckboxHitTestUtils.h
+++ ios/utils/CheckboxHitTestUtils.h
@@ -4,6 +4,12 @@
 
 @interface CheckboxHitTestUtils : NSObject
 
++ (CGPoint)containerPointFromViewPoint:(CGPoint)point
+                              textView:(UITextView *)textView;
+
++ (NSUInteger)glyphIndexAtContainerPoint:(CGPoint)point
+                                textView:(UITextView *)textView;
+
 + (NSInteger)hitTestCheckboxAtPoint:(CGPoint)pt
                             inInput:(EnrichedTextInputView *)input;
 
